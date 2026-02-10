1adeffb7862cef31f5392f9d93449e32831c99af
sgonzalez122 <60163234+sgonzalez122@users.noreply.github.com>
2026-02-10 14:20:03 -0800

    Update submit_transfer_from_dropbox.sh

--- bash/preproc/submit_transfer_from_dropbox.sh
+++ bash/preproc/submit_transfer_from_dropbox.sh
@@ -2,10 +2,10 @@
 #SBATCH --job-name=transfer_data
 #SBATCH --time=48:00:00
 #SBATCH -n 5
-#SBATCH --mail-user=ethanroy@stanford.edu
+#SBATCH --mail-user=sagon151@stanford.edu
 #SBATCH --mail-type=ALL
 #SBATCH --partition=normal,gse
 
 module load system rclone 
 
-rclone copy EdNeuroDropBox:2025_EAR_Groupitizing_EEG/Data/ /scratch/users/ethanroy/eeg_groupitizing_data/raw_data/ --include "*.mat"
+rclone copy EdNeuroDropBox:2025_EAR_Groupitizing_EEG/Data/ /scratch/users/sagon151/eeg-dot-digit-data/raw_data/ --include "*.mat"
